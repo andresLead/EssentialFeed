d446ea8039ebe18a70d1ca72ba4ed57b366628b9
Joan Mazo <joan.mazo@globant.com>
2021-11-03 16:13:49 -0500

    move 'RemoteFeedLoader' component to production.

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,35 @@
+import Foundation
+import XCTest
+import EssentialFeed
+
+final class HTTPClientSpy: HTTPClient {
+    var requestedURL: URL?
+
+    func get(from url: URL) {
+        requestedURL = url
+    }
+}
+
+final class RemoteFeedLoaderTests: XCTestCase {
+    func test_load_doesNotGetURL() {
+        let (_, client) = makeSUT()
+
+        XCTAssertNil(client.requestedURL)
+    }
+
+    func test_load_getURL() {
+        let url = URL(string: "https://given-a-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.load()
+
+        XCTAssertEqual(client.requestedURL, url)
+    }
+
+    // MARK: Helpers
+    private func makeSUT(url: URL = URL(string: "https://an-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+}
