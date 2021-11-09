5301f307d3a075485ac99177ccb497bd2f5978f4
Joan Mazo <joan.mazo@globant.com>
2021-11-09 11:42:55 -0500

    guarantee we don't deliver a result after the 'RemoteFeedLoader' instance has been deallocated.

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -112,6 +112,20 @@ final class RemoteFeedLoaderTests: XCTestCase {
         }
     }
 
+    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+        let url = URL(string: "https://any-url.com")!
+        let client = HTTPClientSpy()
+        var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
+
+        var capturedResults = [Result<[FeedItem], RemoteFeedLoader.Error>]()
+        sut?.load { capturedResults.append($0) }
+
+        sut = nil
+        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
+
+        XCTAssertTrue(capturedResults.isEmpty)
+    }
+
     // MARK: Helpers
     private func makeSUT(url: URL = URL(string: "https://an-url.com")!,
                          file: StaticString = #filePath,
